fc43013149db06943fff9e67f0072bf481836d2d
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-11 19:11:15 +0200

    axios call (not working)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8889/api/projects/';
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+  components: {AppHeader},
+  data: () => ({projects: []}),
+  methods:{
+    fetchProjects(){
+      axios.get(endpoint).then(res => {console.log(res.data)})
+    }
+  },
+  created(){
+    this.fetchProjects();
+  }
 };
 </script>
 
 <template>
-   <h1>ciao</h1>
+  <AppHeader />
+  <main class="container my-5">
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
+  </main>
 </template>
