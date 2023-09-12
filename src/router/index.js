7c4a1b006c1664125bce30b8028282aab6ed3f10
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 19:11:28 +0200

    index updated

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import ContactPage from "../pages/ContactPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
+import ProjectDetailPage from "../pages/ProjectDetailPage.vue";
 
 // create router
 const router = createRouter({
   history: createWebHistory(),
-  linkExactActiveClass: 'active',
+  linkExactActiveClass: "active",
   routes: [
     { path: "/", name: "home", component: HomePage },
+    { path: "/projects/:id", name: "project", component: ProjectDetailPage },
     { path: "/contact", name: "contact", component: ContactPage },
-    { path: '/:not-found', name: 'not-found', component: NotFoundPage},
+    { path: "/:not-found", name: "not-found", component: NotFoundPage },
   ],
 });
 
-export {router}
+export { router };
