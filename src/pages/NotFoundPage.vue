a323cbfa8096e9a9008f3481cf583779b935f5f4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 17:45:19 +0200

    adding also home

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -6,9 +6,10 @@ export default {
 
 <template>
   <div class="not-found-container">
-    <div class="not-found-content">
+    <div class="not-found-content d-flex flex-column">
       <h1>404</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <button class="btn btn-secondary btn-sm my-3" @click="$router.back()">Go back</button>
       <router-link to="/">Go Home</router-link>
     </div>
   </div>
