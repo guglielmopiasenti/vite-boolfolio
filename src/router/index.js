1f2434b2b6a778e09eff01549017ce294631e97b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 16:42:12 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 // importing pages
 
 import HomePage from "../pages/HomePage.vue";
-import ContactsPage from "../pages/ContactsPage.vue";
+import ContactPage from "../pages/ContactPage.vue";
 
 // create router
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomePage },
-    { path: "/contact", name: "contact", component: ContactsPage },
+    { path: "/contact", name: "contact", component: ContactPage },
   ],
 });
