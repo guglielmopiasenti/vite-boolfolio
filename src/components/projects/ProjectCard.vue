7197c3f1d2eaa6eddbd4c4c98c0bbef8aabe8982
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 08:58:17 +0200

    fix and format

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   name: 'ProjectCard',
-  props: {project: Array},
+  props: { project: Array },
   computed: {
     projectDate() {
       const date = new Date(this.project.created_at);
@@ -12,12 +12,13 @@ export default {
       const minute = date.getMinutes();
       const seconds = date.getSeconds();
 
-      day = day < 10 ? '0' = day : day;
-      month = month < 10 ? '0' = month : month;
+      day = day < 10 ? '0' + day : day;
+      month = month < 10 ? '0' + month : month;
+
 
       const projectDate = `${day}/${month}/${year} ${hours}:${minute}:${seconds}`;
 
-      return postDate;
+      return projectDate;
     }
   }
 }
@@ -26,17 +27,17 @@ export default {
 
 <template>
   <section id="project-card">
-    <div class="card my-3">
-     <div class="card-header">
-      <h2>{{ project.name }}</h2>
-     </div>
-     <div class="card-body">
-      <p>{{ project.description }}</p>
-     </div>
-     <div class="card-footer">
-      <div>{{ project.github_url }}</div>
-      <div> Pubblicato il: <time>{{ projectDate }}</time></div>
-     </div>
+    <div class="card my-5">
+      <div class="card-header">
+        <h2>{{ project.name }}</h2>
+      </div>
+      <div class="card-body">
+        <p>{{ project.description }}</p>
+      </div>
+      <div class="card-footer">
+        <div>{{ project.github_url }}</div>
+        <div> Pubblicato il: <time>{{ projectDate }}</time></div>
+      </div>
     </div>
   </section>
 </template>
