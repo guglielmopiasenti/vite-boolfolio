09844f6971acf2c3119bcbea74bc83bc4481715b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 16:48:24 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
-import {router} from router;
+import {router} from './router';
 const app = createApp(App);
 app.use(router);
-app.mount('#app')
+app.mount('#app');
