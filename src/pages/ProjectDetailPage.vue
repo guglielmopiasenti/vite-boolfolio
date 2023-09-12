ad7501918da6886fc42dcc63e1f9f9e8947bac83
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 19:09:27 +0200

    projectdetail created

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,28 @@
+<script>
+import ProjectCard from '../components/projects/ProjectCard.vue';
+const apiBaseUri = 'http://127.0.0.1:8000/api/projects/';
+import axios from 'axios';
+export default {
+  name: 'ProjectDetailPage',
+  components: { ProjectCard },
+  data: () => ({
+    project: null,
+  }),
+  methods: {
+    getProject() {
+      const endpoint = `${apiBaseUri}${this.$route.params.id}`;
+      axios.get(endpoint)
+        .then(res => { this.project = res.data; })
+        .catch(err => { console.error(err) });
+    }
+  },
+  created() {
+    this.getProject();
+  }
+}
+</script>
+
+<template>
+  <h2>Project Details</h2>
+  <ProjectCard v-if="project" :project="project" />
+</template>
