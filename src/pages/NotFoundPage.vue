6df925cfef7b2a732c1de89a5ff377b76d7e66ce
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-12 17:15:49 +0200

    404 page + import

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: 'NotFoundPage'
+}
+</script>
+
+<template>
+  <div class="not-found-container">
+    <div class="not-found-content">
+      <h1>404</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <router-link to="/">Go Home</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.not-found-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.not-found-content {
+  text-align: center;
+  border-radius: 5px;
+  padding: 30px;
+  background-color: #1f1f1f;
+}
+h1 {
+  font-size: 6em;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+
+router-link {
+  padding: 10px 20px;
+  border: 1px solid #ffffff;
+  border-radius: 3px;
+  text-decoration: none;
+  color: #ffffff;
+  transition: background-color 0.2s ease;
+}
+
+router-link:hover {
+  background-color: #ffffff;
+  color: #121212;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../pages/HomePage.vue";
 import ContactPage from "../pages/ContactPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
 // create router
 const router = createRouter({
@@ -10,6 +11,7 @@ const router = createRouter({
   routes: [
     { path: "/", name: "home", component: HomePage },
     { path: "/contact", name: "contact", component: ContactPage },
+    { path: '/:not-found', name: 'not-found', component: NotFoundPage},
   ],
 });
 
